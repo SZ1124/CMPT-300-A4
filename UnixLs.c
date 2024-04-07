6c35e6cc36ca91809eb5c61c51eaaf7ebe2b2948
Mingyang Cheng <163230203+gnaygnim@users.noreply.github.com>
2024-04-06 18:37:10 -0700

    Update UnixLs.c

--- UnixLs.c
+++ UnixLs.c
@@ -12,13 +12,70 @@
 
 #include "list.h"
 
+int temp = 0;
 int flag = 0; // 0 = no flag, 1 = i, 2 = j, 3 = both
 int stage = 0;
-List* directory; // could use a list here.
+List* directoryList; // could use a list here.
+char* current_directory;
+char* parent_directory;
+char* root_directory;
 
 void init()
 {
-    directory = List_create();
+    directoryList = List_create();
+    char cwd[1024];
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        current_directory = malloc(strlen(cwd) + 1);
+        strcpy(current_directory, cwd);
+    } else {
+        perror("getcwd");
+    }
+}
+
+void readDirectory()
+{
+    char* directory_path = List_trim(directoryList);
+    //printf("%s\n", directory_path);
+    DIR* d = opendir(directory_path);
+    if(d == NULL){
+        perror("Error opening directory.\n");
+        return;
+    }
+    struct dirent* dp; 
+    struct stat buf; 
+    while ((dp = readdir(d)) != NULL) {
+        if (strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0 || strcmp(dp->d_name, ".git") == 0) {
+            continue;
+        }
+
+        if (stat(dp->d_name, &buf) == -1) {
+            //perror("Error retrieving file information");
+            continue;
+        }
+        switch(flag){
+            case 1:
+                printf("%ld %s ", buf.st_ino, dp->d_name);
+                break;
+
+            default:
+                break;
+        }
+        // You can access other information from 'buf' as needed
+    }
+    printf("\n");
+
+    closedir(d);
+}
+
+void printDirectories()
+{
+    char* temp;
+    List_first(directoryList);
+    while(List_curr(directoryList) != NULL){
+        temp = List_curr(directoryList);
+        printf("%s\n", temp);
+        List_next(directoryList);
+    }
 }
 
 void readInput()
@@ -31,13 +88,14 @@ void readInput()
     while(1){
         flag = 0;
         stage = 0;
+        temp = 0;
         read = getline(&input, &len, stdin);
         if (read == -1) {
             perror("getline");
             exit(EXIT_FAILURE);
         }
 
-        if (strcmp(input, "!\n") == 0) {
+        if (strcmp(input, "\n") == 0) {
             break;
         } else {
             char* token = strtok(input, delim);
@@ -46,36 +104,48 @@ void readInput()
                 continue;
             } else {
                 while ((token = strtok(NULL, delim)) != NULL) {
-                    // search token for invalid flag, if invalid flag found, treat it as a directory
-                    // the first time a invalid flag is found, stop trying to find flags, all the rest of the arguements become directory
+                    // need to detect, the moment u see 
                     if (token[0] == '-' && stage == 0) {
                         for(int i = 1; i < strlen(token); i++){
                             if(token[i] == 'i'){
-                                //printf("i found at index %d\n", i);
-                                flag |= 1;
+                                temp |= 1;
                             } else if (token[i] == 'l'){
-                                //printf("l found at index %d\n", i);
-                                flag |= 2;
+                                temp |= 2;
                             } else {
                                 stage = 1;
-                                flag = 0;
-                                //printf("directory: %s\n", token);
-                                List_append(directory, token);
+                                temp = 0;
+                                List_prepend(directoryList, token);
+                                break;
                             }
                         }
-                    } else{
-                        //printf("directory: %s\n", token);
-                        List_append(directory, token);
-                    }
+                    } else {
+                        if(strcmp(token, ".") == 0){
+                            List_prepend(directoryList, current_directory);
+                            break;
+                        }
+                        if(strcmp(token, "..") == 0){
+                        }
+                        //printf("%s\n", token);
+                        
+                        List_prepend(directoryList, token);
+                    } 
+                    flag |= temp;
+                }
+                
+                if(List_count(directoryList) == 0){
+                    List_prepend(directoryList, current_directory);
                 }
             }
         }
-        //printf("%d\n",flag);
+        while(List_count(directoryList) != 0){
+            printf("directory: %s\n", (char*)List_curr(directoryList));
+            readDirectory();
+        } 
     }
 }
 
 int main() {
-
+    init();
     readInput();
     return 0;
 
